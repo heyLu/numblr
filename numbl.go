cf2657e6ff57a9d0934abc8649a59e9980b68a8a
Lu Stadler <lu@papill0n.org>
2020-09-08 19:03:24 +0200

    Make reblog avatars in post view a bit prettier

--- numbl.go
+++ numbl.go
@@ -866,6 +866,7 @@ func HandlePost(w http.ResponseWriter, req *http.Request) {
 	<meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1" />
 
 	<style>h1 { word-break: break-all; }blockquote, figure { margin: 0; }blockquote:not(:last-child) { border-bottom: 1px solid #ddd; } blockquote > blockquote:nth-child(1) { border-bottom: 0; }body { font-family: sans-serif; }article{ border-bottom: 1px solid black; padding: 1em 0; }.tags { list-style: none; padding: 0; color: #666; }.tags > li { display: inline }img, video, iframe { max-width: 95vw; }@media (min-width: 60em) { body { margin-left: 15vw; } article { max-width: 60em; } img, video { max-height: 20vh; width: auto; } img:hover, video:hover { max-height: 100%%; }}.avatar{height: 1em;}a.author,a.author:visited{color: #000;}%s</style>
+	<style>.post-reblog-header img { height: 1em; vertical-align: middle; }.post-reblog-header .post-avatar { display: inline-block; }.post-reblog-header .post-tumblelog-name:after { content: ":"; }</style>
 </head>
 
 <body>
