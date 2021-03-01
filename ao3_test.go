6f6e7acae7ad82a684d960dc958d18eb2c151755
Luna Stadler <lu@papill0n.org>
2021-03-01 20:48:50 +0100

    Fix tests for link in ao3 titles

--- ao3_test.go
+++ ao3_test.go
@@ -20,7 +20,7 @@ func TestAO3AuthorFandomFeed(t *testing.T) {
 	assert.Equal(t, "https://archiveofourown.org/works/7756009", post.URL, "url")
 	assert.Equal(t, "13 Aug 2016", post.DateString, "date string")
 	assert.Equal(t, time.Date(2016, time.August, 13, 0, 0, 0, 0, time.UTC), post.Date, "date")
-	assert.Equal(t, "<h1>[VID] You Are A Runner And I Am My Father's Son by astolat</h1>", post.Title, "title")
+	assert.Equal(t, "<h1><a href=\"https://archiveofourown.org/works/7756009\">[VID] You Are A Runner And I Am My Father's Son</a> by astolat</h1>", post.Title, "title")
 	assert.Equal(t, "astolat", post.Author, "author")
 	assert.Equal(t, "<p>I&#39;ll draw three figures on your heart.</p>", post.DescriptionHTML, "description")
 	assert.Equal(t, []string{"Creator Chose Not To Use Archive Warnings", "Draco Malfoy/Harry Potter", "Draco Malfoy", "Harry Potter", "Vividcon", "Vividcon 2016", "Vividcon 2016 Premieres"}, post.Tags, "tags")
