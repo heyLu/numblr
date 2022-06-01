dec86a2a38d3085ae17cb03db29ca6afbdcc38e2
Luna Stadler <lu@papill0n.org>
2022-06-01 21:52:53 +0200

    Fix front page crashing sometimes (e.g. for /random)

--- numblr.go
+++ numblr.go
@@ -779,7 +779,11 @@ func HandleTumblr(w http.ResponseWriter, req *http.Request) {
 			if avatarURL == "" {
 				avatarURL = "/avatar/" + post.Author
 			}
-			fmt.Fprintf(w, `<p><img class="avatar" src="%s" loading="lazy" /> <a class="author" title=%q href="/%s">%s</a>:</p>`, avatarURL, html.EscapeString(feedInfo[post.Author].Feed.Description()), post.Author, post.Author)
+			feedDescription := ""
+			if feedInfo[post.Author].Feed != nil {
+				feedDescription = feedInfo[post.Author].Feed.Description()
+			}
+			fmt.Fprintf(w, `<p><img class="avatar" src="%s" loading="lazy" /> <a class="author" title=%q href="/%s">%s</a>:</p>`, avatarURL, html.EscapeString(feedDescription), post.Author, post.Author)
 
 			if len(post.Tags) > 0 {
 				fmt.Fprint(w, `<ul class="tags content-notes">`)
