37661346ed1f74f89f548f86394498e7d120727a
Luna Stadler <lu@papill0n.org>
2022-06-06 17:15:17 +0200

    Fix missing import in tests

--- numblr_test.go
+++ numblr_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"testing"
 
+	"github.com/heyLu/numblr/feed"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -10,12 +11,12 @@ func TestNextPostsGroup(t *testing.T) {
 	testCases := []struct {
 		author            string
 		groupSize         int
-		posts             []*Post
+		posts             []*feed.Post
 		numGroup, numRest int
 	}{
-		{"a", 3, []*Post{&Post{Author: "a"}, &Post{Author: "a"}, &Post{Author: "a"}, &Post{Author: "b"}, &Post{Author: "c"}, &Post{Author: "d"}}, 3, 3},
-		{"a", 3, []*Post{&Post{Author: "a"}, &Post{Author: "x"}, &Post{Author: "a"}, &Post{Author: "b"}, &Post{Author: "c"}, &Post{Author: "d"}}, 1, 5},
-		{"a", 3, []*Post{&Post{Author: "a"}, &Post{Author: "a"}, &Post{Author: "a"}}, 3, 0},
+		{"a", 3, []*feed.Post{{Author: "a"}, {Author: "a"}, {Author: "a"}, {Author: "b"}, {Author: "c"}, {Author: "d"}}, 3, 3},
+		{"a", 3, []*feed.Post{{Author: "a"}, {Author: "x"}, {Author: "a"}, {Author: "b"}, {Author: "c"}, {Author: "d"}}, 1, 5},
+		{"a", 3, []*feed.Post{{Author: "a"}, {Author: "a"}, {Author: "a"}}, 3, 0},
 	}
 
 	for _, tc := range testCases {
