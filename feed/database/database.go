c55a1d041731c581828fd92729a85fa234978717
Luna Stadler <lu@papill0n.org>
2023-07-22 18:15:17 +0200

    Switch back to `wal_checkpoint(PASSIVE)` to prevent "database is locked" errs
    This may cause `.wal` growth again, let's see how well it works in
    practice

--- feed/database/database.go
+++ feed/database/database.go
@@ -57,7 +57,7 @@ func InitDatabase(dbPath string) (*sql.DB, error) {
 		for {
 			time.Sleep(10 * time.Minute)
 
-			_, err := db.ExecContext(context.Background(), "PRAGMA wal_checkpoint(truncate)")
+			_, err := db.ExecContext(context.Background(), "PRAGMA wal_checkpoint(PASSIVE)")
 			if err != nil {
 				log.Printf("Error: wal checkpoint failed: %s", err)
 				continue
--- feed/database/database_test.go
+++ feed/database/database_test.go
@@ -1,9 +1,20 @@
 package database
 
 import (
+	"context"
+	"errors"
 	"fmt"
+	"io"
+	"math/rand"
 	"net"
+	"path"
+	"sync"
 	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/heyLu/numblr/feed"
 )
 
 func TestIsTimeoutError(t *testing.T) {
@@ -23,4 +34,75 @@ func TestIsTimeoutError(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestConcurrentWrites(t *testing.T) {
+	dbDir := t.TempDir()
+	db, err := InitDatabase(path.Join(dbDir, "cache.db"))
+	require.NoError(t, err)
+	defer db.Close()
+
+	feeds := []string{"staff", "engineering", "changes", "love", "fun", "transtrucks"}
+
+	rnd := rand.New(rand.NewSource(time.Now().UnixNano()))
+
+	var wg sync.WaitGroup
+	fetchFeeds := func(feedName string) {
+		defer wg.Done()
+
+		if feedName == "" {
+			feedName = feeds[rnd.Intn(len(feeds))]
+		}
+
+		start := time.Now()
+		feed, err := OpenCached(context.Background(), db, feedName, fakeOpen, feed.Search{ForceFresh: true})
+		require.NoError(t, err)
+
+		defer feed.Close()
+
+		n := 0
+		_, err = feed.Next()
+		for err == nil {
+			n++
+			_, err = feed.Next()
+		}
+		t.Log(feedName, n, time.Since(start))
+		if err != nil && !errors.Is(err, io.EOF) {
+			require.NoError(t, err)
+		}
+
+		defer time.Sleep(time.Duration(rnd.Intn(100)) * time.Millisecond)
+	}
+
+	// warm up
+	// for i := 0; i < len(feeds); i++ {
+	// 	wg.Add(1)
+	// 	fetchFeeds(feeds[i])
+	// }
+	// t.Log("warmup done")
+
+	go func() {
+		for {
+			// "TRUNCATE" causes "database is locked" errors, "PASSIVE" does not
+			_, err := db.ExecContext(context.Background(), "PRAGMA wal_checkpoint(PASSIVE)")
+			if err != nil {
+				continue
+			}
+		}
+	}()
+
+	// concurrent
+	for i := 0; i < 50; i++ {
+		wg.Add(1)
+		go fetchFeeds("")
+	}
+
+	wg.Wait()
+}
+
+func fakeOpen(ctx context.Context, name string, _ feed.Search) (feed.Feed, error) {
+	time.Sleep(100 * time.Millisecond)
+	return &feed.Static{Posts: []feed.Post{
+		{ID: "xyz", Author: name},
+	}}, nil
 }
